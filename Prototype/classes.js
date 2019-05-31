c07c245900152168cb317d71956aa176fd00a158
Bob Huang <bobhuang123456789@gmail.com>
2019-05-31 16:08:55 -0400

    added basic methods

--- Prototype/classes.js
+++ Prototype/classes.js
@@ -1,19 +1,44 @@
-
 // Element class (node)
 class Element {
 
+	// initialize Element (str, str)
+	constructor(name, symbol) {
+		this.name = name;
+		this.symbol = symbol;
+		this.links = [];
+	}
+
+	link(element) {
+		this.links.push(element);
+	}
+
+	print() {
+		let string = this.symbol + " :";
+
+		for (let i = 0; i < this.links.length; i++) {
+			string += " " + this.links[i].symbol;
+		}
+
+		console.log(string);
+	}
+}
+
+class parser {
+	// utility class for parsing
+	// use static methods
 }
 
 // Compound class (graph)
 class Compound {
 
-	// initialize Compound
+	// initialize Compound (str)
 	constructor(name) {
 		this.name = name;
 	}
 
 	create() {
 		// parses name and creates compound as a graph where nodes are elements
+		// should be a list of elements (where the elements link forms a graph)
 	}
 
 	print() {
@@ -21,3 +46,15 @@ class Compound {
 	}
 
 }
+
+let a = new Element("a", "A");
+let b = new Element("b", "B");
+let c = new Element("c", "C");
+
+a.link(b);
+a.link(c);
+c.link(b);
+
+a.print();
+b.print();
+c.print();
