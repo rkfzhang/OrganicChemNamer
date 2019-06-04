e939ad69e3bf80a7900b0be151273d6f9a1b54f6
Bob Huang <bobhuang123456789@gmail.com>
2019-06-04 16:55:21 -0400

    Works with single, double, and triple carbon bonds

--- Prototype/classes.js
+++ Prototype/classes.js
@@ -26,6 +26,9 @@ class Element {
 class parser {
 	// utility class for parsing
 	// use static methods
+	static split(element, string) {
+
+	}
 }
 
 // Compound class (graph)
@@ -39,10 +42,56 @@ class Compound {
 	create() {
 		// parses name and creates compound as a graph where nodes are elements
 		// should be a list of elements (where the elements link forms a graph)
+
+		const section = this.name.split("-");
+		let index = section.length - 1;
+
+		// check for triple bond
+		if (section[index] === "yne") {
+			var longestChain = section[index - 2];
+			var tripleBond = section[index - 1] //ie [2,3]
+			index -= 2;
+		}
+		else {
+			var longestChain = section[index];
+			var tripleBond = -1;
+		}
+
+		// check for double bond
+		const strChainLength = longestChain.length;
+
+		if (longestChain.substring(strChainLength - 3, strChainLength) === "ene") {
+			var doubleBond = section[index - 1]; //ie [2,3]
+		}
+		else {
+			var doubleBond = -1;
+		}
+
+		// check for carbon chain length
+		const carbonSuffix = ["meth", "eth", "prop", "but", "pent", "hex", "hept", "oct", "non", "dec"];
+		const carbonSuffixLength = carbonSuffix.length;
+
+		for (let i = 0; i < carbonSuffixLength; i++) {
+		
+			if (carbonSuffix[i] === longestChain.substring(0, carbonSuffix[i].length)) {
+				var carbonChain = i + 1;
+				break;
+			}
+		}
+
+		console.log("Name:");
+		console.log(this.name);
+		console.log("Triple bond:");
+		console.log(tripleBond);
+		console.log("Double bond:");
+		console.log(doubleBond);
+		console.log("Carbon length:");
+		console.log(carbonChain);
+
 	}
 
 	print() {
-		console.log(name);
+		console.log(this.name);
 	}
 
 }
@@ -58,3 +107,6 @@ c.link(b);
 a.print();
 b.print();
 c.print();
+
+let compTest = new Compound("1,3-hexdiene-5-yne");
+compTest.create();
